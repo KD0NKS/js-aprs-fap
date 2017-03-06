746319b4c6c5c58da0a3752d7396c2503ee271d5
codeape <andrew.m.fairhurst@gmail.com>
2017-03-05 20:35:34 -0600

    Config changes for import usage.

---
+++ index.ts
@@ -0,0 +1,13 @@
+import aprsPacket from './src/aprsPacket';
+import aprsParser from './src/parser';
+import digipeater from './src/digipeater';
+import telemetry from './src/telemetry';
+import wx from './src/wx';
+
+export {
+   aprsPacket
+   , aprsParser
+   , digipeater
+   , telemetry
+   , wx
+};

25657bb0d60ed532362d40c04cbcf20861003944
KD0NKS <KD0NKS@users.noreply.github.com>
2021-03-23 21:40:47 -0500

    2021 03 23 updates (#42)
    * * Added id default null id to aprspacket.
    * Added default date now timestamp to aprspacket.
    * DST symbols are now in their own file.
    * Result messages are now in their own file.
    * Added a couple more test scenarios.
    
    * * Updated test versions.
    * Updated dependencies.

--- test/40decode-object-inv.test.ts
+++ test/40decode-object-inv.test.ts
@@ -4,49 +4,53 @@
  * a cut 'n paste operation
  * Tue Dec 11 2007, Hessu, OH7LZB
  */
+import aprsPacket from '../src/aprsPacket';
+import aprsParser from '../src/parser';
 import * as chai from 'chai';
 
 const assert = chai.assert;
 
-import aprsPacket from '../src/aprsPacket';
-import aprsParser from '../src/parser';
-
 describe('FAP - Test parsing a bad packet', function() {
-    let parser: aprsParser = new aprsParser();
+    const parser: aprsParser = new aprsParser();
 
     describe('#parseaprs - Test parsing a bad packet', function() {
-        let $srccall = "OH2KKU-1";
-        let $dstcall = "APRS";
-
-        let $aprspacket = `${$srccall}>${$dstcall},TCPIP*,qAC,FIRST:;SRAL HQ *110507zS0%E/Th4_a AKaupinmaenpolku9,open M-Th12-17,F12-14 lcl`;
-
-        let parsed: aprsPacket = parser.parseaprs($aprspacket);
+        const $aprspacket = `OH2KKU-1>APRS,TCPIP*,qAC,FIRST:;SRAL HQ *110507zS0%E/Th4_a AKaupinmaenpolku9,open M-Th12-17,F12-14 lcl`
+        const parsed: aprsPacket = parser.parseaprs($aprspacket)
 
         it('Should return a resultcode: obj_inv', function() {
             assert.equal('obj_inv', parsed.resultCode);
-        });
+        })
 
         it('Should return a type: object', function() {
             assert.equal('object', parsed.type);
-        });
-    });
+        })
+    })
 
     // Note: the perl version will never hit this scenario as it dumps out when trying to decide what type of packet it is
     // because it is less than 31 characters
     describe('#parseaprs - Test parsing a bad packet', function () {
-        let $srccall = "OH2KKU-1";
-        let $dstcall = "APRS";
-
-        let $aprspacket = `${$srccall}>${$dstcall}:;110507zS0%E/Th4_a AK`;
-
-        let parsed: aprsPacket = parser.parseaprs($aprspacket, { isax25: true });
+        const $aprspacket = `OH2KKU-1>APRS:;110507zS0%E/Th4_a AK`
+        const parsed: aprsPacket = parser.parseaprs($aprspacket, { isax25: true })
 
         it('Should return a resultcode: obj_short', function () {
             assert.equal('obj_short', parsed.resultCode);
-        });
+        })
 
         it('Should return a type: object', function () {
             assert.equal('object', parsed.type);
-        });
-    });
-});
+        })
+    })
+
+    describe('#parseaprs - Test parsing a bad packet', function () {
+        const $aprspacket = `K8ETN-S>APJIO4,TCPIP*,qAC,K8ETN-GS:;K8ETN  C *240015h    .  ND     .  EaRNG0045 2m Voice 145.200 -0.600 MHz`
+        const parsed: aprsPacket = parser.parseaprs($aprspacket)
+
+        it('Should return a warning code \'timestamp_inv_obj', function () {
+            assert.equal('timestamp_inv_obj', parsed.warningCodes[0])
+        })
+
+        it('Should return a timestamp: 0', function () {
+            assert.equal(0, parsed.timestamp)
+        })
+    })
+})
--- test/56decode-status.test.ts
+++ test/56decode-status.test.ts
@@ -1,38 +1,63 @@
-const assert = require('assert');
+import aprsPacket from '../src/aprsPacket'
+import aprsParser from '../src/parser'
+import * as chai from 'chai';
+import { RESULT_MESSAGES } from '../src/ResultMessages'
+import assert = require('assert')
 
-import aprsPacket from '../src/aprsPacket';
-import aprsParser from '../src/parser';
+const should = chai.should();
 
 describe('FAP - Status message decoding', function() {
-    let parser = new aprsParser();
+    let parser = new aprsParser()
 
     describe('#parseaprs - Status message\'s timestamp is not affected by the raw_timestamp flag.', function() {
-        let $now = new Date();
-        $now.setMilliseconds(0); // null out milliseconds as they aren't taken into consideration during parsing
-        $now.setSeconds(0);
+        let $now = new Date()
+        $now.setMilliseconds(0) // null out milliseconds as they aren't taken into consideration during parsing
+        $now.setSeconds(0)
 
         let $tstamp = ('00'.substring(0, 2 - $now.getUTCDate().toString().length) + $now.getUTCDate())
                 + ('00'.substring(0, 2 - $now.getUTCHours().toString().length) + $now.getUTCHours())
                 + ('00'.substring(0, 2 - $now.getUTCMinutes().toString().length) + $now.getUTCMinutes())
-                + 'z';
+                + 'z'
 
-        let nowTime = Math.floor($now.getTime() / 1000);
-        let outcome = nowTime - (nowTime % 60);
-        let $msg = '>>Nashville,TN>>Toronto,ON';
-        let $aprspacket = 'KB3HVP-14>APU25N,WIDE2-2,qAR,LANSNG:>' + $tstamp + $msg;
+        let nowTime = Math.floor($now.getTime() / 1000)
+        let outcome = nowTime - (nowTime % 60)
+        let $msg = '>>Nashville,TN>>Toronto,ON'
+        let $aprspacket = 'KB3HVP-14>APU25N,WIDE2-2,qAR,LANSNG:>' + $tstamp + $msg
 
-        let parsed: aprsPacket = parser.parseaprs($aprspacket, { 'raw_timestamp': 1 });
+        let parsed: aprsPacket = parser.parseaprs($aprspacket, { 'raw_timestamp': 1 })
 
         it('Should return the type: status', function() {
-            assert.equal('status', parsed.type);
-        });
+            assert.equal('status', parsed.type)
+        })
 
         it('Should return the timestamp: ' + outcome, function() {
-            assert.equal(outcome, parsed.timestamp);
-        });
+            assert.equal(outcome, parsed.timestamp)
+        })
 
         it('Should return the status: ' + $msg, function() {
-            assert.equal($msg, parsed.status);
-        });
+            assert.equal($msg, parsed.status)
+        })
     });
+
+    describe('#parseaprs - Status message\'s timestamp is invalid.', function () {
+        let $aprspacket = 'KB3HVP-14>APU25N,WIDE2-2,qAR,LANSNG:>000000z>>Nashville,TN>>Toronto,ON'
+        let parsed: aprsPacket = parser.parseaprs($aprspacket, { 'raw_timestamp': 1 })
+
+        it('Should return a warning code \'timestamp_inv_sta', function () {
+            assert.equal('timestamp_inv_sta', parsed.warningCodes[0])
+        })
+
+        it(`Should return a resultMessage: ${ RESULT_MESSAGES['timestamp_inv_sta'] }`, function() {
+            assert.equal(RESULT_MESSAGES['timestamp_inv_sta'], parsed.resultMessage)
+        })
+    })
+
+    describe('#parseaprs - Status message has no timestamp', function () {
+        let $aprspacket = 'KB3HVP-14>APU25N,WIDE2-2,qAR,LANSNG:>Nashville,TN>>Toronto,ON'
+        let parsed: aprsPacket = parser.parseaprs($aprspacket, { 'raw_timestamp': 1 })
+
+        it('Should not have a timestamp', function () {
+            should.not.exist(parsed.timestamp)
+        })
+    })
 });

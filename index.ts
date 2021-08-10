c235e6d1100f925a4c80efaa5333993f75c0ed9c
KD0NKS <codegorilla84@gmail.com>
2021-08-09 19:34:52 -0500

    Adding conversion util back to exports.

--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@ import { ConversionConstantEnum } from './src/ConversionConstantEnum'
 import { PacketTypeEnum } from './src/PacketTypeEnum'
 import digipeater from './src/digipeater'
 import telemetry from './src/telemetry'
-//import { ConversionUtil } from './src/utils/ConversionUtil'
+import { ConversionUtil } from './src/ConversionUtil'
 import wx from './src/wx'
 
 export {
     aprsPacket
     , aprsParser
     , ConversionConstantEnum
-//    , ConversionUtil
+    , ConversionUtil
     , digipeater
     , PacketTypeEnum
     , telemetry
